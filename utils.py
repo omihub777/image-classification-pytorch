155bb29da8a4bdfe69e801105d78d1dc51e4ef8d
omihub777 <77219025+omihub777@users.noreply.github.com>
2021-02-04 01:07:07 +0900

    [UPD] Dataset for C100

--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ def get_model(args):
 def get_transform(args):
     train_transform = []
     test_transform = []
-    if args.dataset == 'c10':
+    if args.dataset == 'c10' or 'c100':
         train_transform.append(transforms.RandomCrop(size=args.size, padding=args.padding))
         train_transform.append(transforms.RandomHorizontalFlip())
 
@@ -31,12 +31,22 @@ def get_transform(args):
 def get_dataset(args):
     root = "data"
     if args.dataset == "c10":
+        args.num_classes=10
         args.size = 32
         args.padding = 4
         args.mean, args.std = [0.4914, 0.4822, 0.4465], [0.2470, 0.2435, 0.2616]
         train_transform, test_transform = get_transform(args)
         train_ds = torchvision.datasets.CIFAR10(root, train=True, transform=train_transform, download=True)
         test_ds = torchvision.datasets.CIFAR10(root, train=False, transform=test_transform, download=True)
+
+    if args.dataset == "c100":
+        args.num_classes=100
+        args.size = 32
+        args.padding = 4
+        args.mean, args.std = [0.5071, 0.4867, 0.4408], [0.2675, 0.2565, 0.2761]
+        train_transform, test_transform = get_transform(args)
+        train_ds = torchvision.datasets.CIFAR100(root, train=True, transform=train_transform, download=True)
+        test_ds = torchvision.datasets.CIFAR100(root, train=False, transform=test_transform, download=True)
 
     else:
         raise NotImplementedError(f"{args.dataset} is not implemented yet.")
